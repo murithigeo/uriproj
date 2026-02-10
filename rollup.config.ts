b8fda858d3645aa8065afaf4d83d34509dc35b24
Edwin Gichuru <murithiedwing@gmail.com>
2026-02-10 21:58:51 +0300

    Add npm pub workflow, add babel,node-resolve, remove cjs output, config to es6

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,32 +1,28 @@
 import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
 import { defineConfig } from "rollup";
 
 const sourcemap = true;
 export default defineConfig({
   external: ["proj4"],
   input: "./src/index.ts",
-  plugins: [typescript()],
+  plugins: [typescript(), resolve(), babel({ babelHelpers: "bundled" })],
   output: [
     {
       format: "es",
       name: "uriproj",
-      file: "./dist/uriproj.esm.js",
-      sourcemap,
-    },
-    {
-      format: "commonjs",
-      name: "uriproj",
-      file: "./dist/uriproj.cjs.js",
+      file: "./dist/uriproj.js",
       sourcemap,
     },
     {
       format: "iife",
       name: "uriproj",
-      file: "dist/uriproj.browser.js",
-      globals: { proj4: "proj4" },
+      file: "./dist/uriproj.browser.js",
       sourcemap,
       plugins: [terser()],
+      globals: { proj4: "proj4" },
     },
   ],
 });
